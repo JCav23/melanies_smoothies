21a5f895e2296d666cbfa39903b48f0d217d3182
Jack Cavanagh <jack.m.cavanagh@hotmail.co.uk>
2025-02-11 14:27:38 +0000

    Update streamlit_app.py
    removed pandas dataframe display

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,7 +22,7 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 
 # Convert the Snowpark DataFrame to a pandas Dataframe so we can use the LOC function
 pd_df = my_dataframe.to_pandas()
-st.dataframe(pd_df)
+# st.dataframe(pd_df)
 # st.stop()
 
 ingredients_list = st.multiselect(
@@ -42,7 +42,7 @@ if ingredients_list:
         
         st.subheader(fruit_chosen + ' Nutrition Information')
         fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + search_on)
-        # sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
     st.write(ingredients_string)
 
